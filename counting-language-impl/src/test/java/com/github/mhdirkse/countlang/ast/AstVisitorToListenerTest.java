e2388e55381e2734cbcc2fbe409089f7dba6adf8
martijn <mhdirkse@live.nl>
2018-09-05 10:57:24 +0200

    Test AstVisitorToListener.

---
+++ counting-language-impl/src/test/java/com/github/mhdirkse/countlang/ast/AstVisitorToListenerTest.java
@@ -0,0 +1,65 @@
+package com.github.mhdirkse.countlang.ast;
+
+import org.easymock.EasyMockRunner;
+import org.easymock.Mock;
+import org.easymock.MockType;
+import org.junit.runner.RunWith;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.easymock.EasyMock.*;
+
+@RunWith(EasyMockRunner.class)
+public class AstVisitorToListenerTest extends AstConstructionTestBase {
+    @Mock(type = MockType.STRICT)
+    AstListener listener;
+
+    private void runProgram(final String programText) {
+        parse(programText);
+        Assert.assertFalse(hasParseErrors);
+        AstVisitorToListener instance = new AstVisitorToListener(listener);
+        ast.accept(instance);
+    }
+
+    @Test
+    public void testPrintOperatorExpression() {
+        listener.enterProgram(isA(Program.class));
+        listener.enterPrintStatement(isA(PrintStatement.class));
+        listener.enterCompositeExpression(isA(CompositeExpression.class));
+        listener.visitOperator(isA(Operator.class));
+        listener.visitValueExpression(isA(ValueExpression.class));
+        listener.visitValueExpression(isA(ValueExpression.class));
+        listener.exitCompositeExpression(isA(CompositeExpression.class));
+        listener.exitPrintStatement(isA(PrintStatement.class));
+        listener.exitProgram(isA(Program.class));
+        replay(listener);
+        runProgram("print 5 + 3");
+        verify(listener);
+    }
+
+    @Test
+    public void testFunctionDefAndFunctionCallAndAssignment() {
+        listener.enterProgram(isA(Program.class));
+        listener.enterFunctionDefinitionStatement(isA(FunctionDefinitionStatement.class));
+        listener.enterFormalParameters(isA(FormalParameters.class));
+        listener.visitFormalParameter(isA(FormalParameter.class));
+        listener.exitFormalParameters(isA(FormalParameters.class));
+        listener.enterReturnStatement(isA(ReturnStatement.class));
+        listener.enterCompositeExpression(isA(CompositeExpression.class));
+        listener.visitOperator(isA(Operator.class));
+        listener.visitSymbolExpression(isA(SymbolExpression.class));
+        listener.visitValueExpression(isA(ValueExpression.class));
+        listener.exitCompositeExpression(isA(CompositeExpression.class));
+        listener.exitReturnStatement(isA(ReturnStatement.class));
+        listener.exitFunctionDefinitionStatement(isA(FunctionDefinitionStatement.class));
+        listener.enterAssignmentStatement(isA(AssignmentStatement.class));
+        listener.enterFunctionCallExpression(isA(FunctionCallExpression.class));
+        listener.visitValueExpression(isA(ValueExpression.class));
+        listener.exitFunctionCallExpression(isA(FunctionCallExpression.class));
+        listener.exitAssignmentStatement(isA(AssignmentStatement.class));
+        listener.exitProgram(isA(Program.class));
+        replay(listener);
+        runProgram("function fun(x) {return x + 2}; y = fun(3)");
+        verify(listener);
+    }
+}
