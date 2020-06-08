45d6eeaf6e1b8e8d3fbd611502eb73a867d1de9d
Martijn Dirkse <mhdirkse@live.nl>
2020-06-08 19:40:18 +0200

    Merge AbstractStatementGroupHandler and StatementGroupHandler

--- counting-language-impl/src/main/java/com/github/mhdirkse/countlang/lang/parsing/StatementGroupHandler.java
+++ counting-language-impl/src/main/java/com/github/mhdirkse/countlang/lang/parsing/StatementGroupHandler.java
@@ -1,9 +1,14 @@
 package com.github.mhdirkse.countlang.lang.parsing;
 
 import com.github.mhdirkse.countlang.ast.StatementGroup;
+import com.github.mhdirkse.countlang.lang.CountlangParser;
+
+import org.antlr.v4.runtime.misc.NotNull;
+
+import com.github.mhdirkse.codegen.runtime.HandlerStackContext;
 import com.github.mhdirkse.countlang.ast.Statement;
 
-class StatementGroupHandler extends AbstractStatementGroupHandler {
+class StatementGroupHandler extends AbstractCountlangListenerHandler {
     private StatementGroup statementGroup;
 
     StatementGroup getStatementGroup() {
@@ -11,11 +16,78 @@ class StatementGroupHandler extends AbstractStatementGroupHandler {
     }
 
     StatementGroupHandler(final int line, final int column) {
+        super(false);
         statementGroup = new StatementGroup(line, column);
     }
 
     @Override
-    void addStatement(final Statement statement) {
-        statementGroup.addStatement(statement);
+    public boolean enterPrintStatement(
+            @NotNull CountlangParser.PrintStatementContext antlrCtx, HandlerStackContext<CountlangListenerHandler> delegationCtx) {
+        int line = antlrCtx.start.getLine();
+        int column = antlrCtx.start.getCharPositionInLine();
+        delegationCtx.addFirst(new PrintStatementHandler(line, column));
+        return true;
+    }
+
+    @Override
+    public boolean enterAssignmentStatement(
+            @NotNull CountlangParser.AssignmentStatementContext antlrCtx, HandlerStackContext<CountlangListenerHandler> delegationCtx) {
+        int line = antlrCtx.start.getLine();
+        int column = antlrCtx.start.getCharPositionInLine();
+        delegationCtx.addFirst(new AssignmentStatementHandler(line, column));
+        return true;
+    }
+
+    @Override
+    public boolean enterReturnStatement(
+            @NotNull CountlangParser.ReturnStatementContext ctx, HandlerStackContext<CountlangListenerHandler> delegationCtx) {
+        int line = ctx.start.getLine();
+        int column = ctx.start.getCharPositionInLine();
+        delegationCtx.addFirst(new ReturnStatementHandler(line, column));
+        return true;
+    }
+
+    @Override
+    public boolean enterFunctionDefinitionStatement(
+            CountlangParser.FunctionDefinitionStatementContext antlrCtx, HandlerStackContext<CountlangListenerHandler> delegationCtx) {
+        int line = antlrCtx.start.getLine();
+        int column = antlrCtx.start.getCharPositionInLine();
+        delegationCtx.addFirst(new FunctionDefinitionStatementHandler(line, column));
+        return true;
+    }
+
+    @Override
+    public boolean exitPrintStatement(
+            @NotNull CountlangParser.PrintStatementContext antlrCtx, HandlerStackContext<CountlangListenerHandler> delegationCtx) {
+        return handleStatementExit(delegationCtx);
+    }
+
+    private boolean handleStatementExit(HandlerStackContext<CountlangListenerHandler> delegationCtx) {
+        if (delegationCtx.isFirst()) {
+            return false;
+        } else {
+            Statement statement = ((StatementSource) delegationCtx.getPreviousHandler()).getStatement();
+            statementGroup.addStatement(statement);
+            delegationCtx.removeAllPreceeding();
+            return true;
+        }
+    }
+
+    @Override
+    public boolean exitAssignmentStatement(
+            @NotNull CountlangParser.AssignmentStatementContext antlrCtx, HandlerStackContext<CountlangListenerHandler> delegationCtx) {
+        return handleStatementExit(delegationCtx);
+    }
+
+    @Override
+    public boolean exitReturnStatement(
+            @NotNull CountlangParser.ReturnStatementContext ctx, HandlerStackContext<CountlangListenerHandler> delegationCtx) {
+        return handleStatementExit(delegationCtx);
+    }
+
+    @Override
+    public boolean exitFunctionDefinitionStatement(
+            @NotNull CountlangParser.FunctionDefinitionStatementContext antlrCtx, HandlerStackContext<CountlangListenerHandler> delegationCtx) {
+        return handleStatementExit(delegationCtx);
     }
 }
